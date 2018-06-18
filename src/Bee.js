f3d653ca24b32946f75a1a1d3d01d9227ac1b831
Joyce Cho <joyjoycho@gmail.com>
2018-06-18 12:42:58 -0700

    completed Bee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,9 @@
 var Bee = function() {
+  Grub.call(this,) // this call Grub instructor
+  this.age = 5; // update new value;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
